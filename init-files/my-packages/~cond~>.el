bcc4f28b5df82aa0de436109e3722a6ccc35ee60
Alkindar Ferraz <alferraz@uolinc.com>
2021-05-12 14:51:42 -0300

    Remove comments

--- init-files/my-packages/~cond~>.el
+++ init-files/my-packages/~cond~>.el
@@ -2,15 +2,6 @@
 ;;; Commentary:
 ;; This package implements clojure' cond~> threading macro
 
-
-;;; 2
-;; da forma como está implementado em clj
-;; cada par (teste exp) é tranformado em uma closure com uma variavel generica g
-;; dps g é associado ao retorno da primeira expressão e assim sucessivamente
-
-;; quando expandido e entao rodado, o valor está correto
-;; se rodar sem expandir apenas o ultimo teste é executado
-;; steps possui ~>
 (defmacro ~cond~> (x &rest clauses)
   "Conditionally thread X through CLAUSES.
 Threads x (via `~>') through each form for which the
